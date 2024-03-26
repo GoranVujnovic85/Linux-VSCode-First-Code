18421c81105214d02ca799f62464b1ac23256ae9
GoranVujnovic85 <goranvujnovic85@gmail.com>
2024-03-26 12:30:06 +0100

    by means of semaphore

--- Main.c
+++ Main.c
@@ -1,23 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-static pthread_mutex_t cs_mutex;
+#define MAXVALUE 3
 
+static sem_t startSemaphore1;
+static sem_t startSemaphore2;
+static sem_t startSemaphore3;
+
+static pthread_t hPrint1;
+static pthread_t hPrint2;
+static pthread_t hPrint3;
 
 void* print1 (void *pParam)
 {
-    int i;
-    for (i = 0 ; i < 3 ; i++)
+    sem_wait(&startSemaphore1);
+
+    /* prikljuci se na na nit 2*/
+    pthread_join(hPrint2, NULL);
+
+    for (int i = 0; i < MAXVALUE; i++)
     {
-        pthread_mutex_lock(&cs_mutex);
-
-            printf("Podesavanje");
-            printf(" GIT-a");
-            printf(" i");
-            printf(" VSCoda\n");
-
-        pthread_mutex_unlock(&cs_mutex);
+        printf("Podesavanje");
+        printf(" GIT-a");
+        printf(" i");
+        printf(" VSCoda\n");    
     }
 
     return 0;
@@ -25,16 +33,16 @@ void* print1 (void *pParam)
 
 void* print2 (void *lpParam)
 {
-    int i;
-    for (i = 0 ; i < 6; i++)
+    sem_wait(&startSemaphore2);
+
+    /* prikljuci se na na nit 3*/
+    pthread_join(hPrint3, NULL);
+
+    for (int i = 0; i < MAXVALUE; i++)
     {
-        pthread_mutex_lock(&cs_mutex);
-
-            printf("Pozdrav");
-            printf(" iz");
-            printf(" Linux-a\n");
-
-        pthread_mutex_unlock(&cs_mutex);
+        printf("Pozdrav");
+        printf(" iz");
+        printf(" Linux-a\n");   
     }
 
     return 0;
@@ -42,19 +50,16 @@ void* print2 (void *lpParam)
 
 void* print3 (void *lpParam)
 {
-    int i;
-    for(i = 0 ; i < 9 ; i++)
+    sem_wait(&startSemaphore3);
+
+    for(int i = 0; i < MAXVALUE; i++)
     {
-        pthread_mutex_lock(&cs_mutex);
-
-            printf("Danas");
-            printf(" je");
-            printf(" petak");
-            printf(" moj");
-            printf(" omiljeni");
-            printf(" dan \n");
-
-        pthread_mutex_unlock(&cs_mutex);
+        printf("Danas");
+        printf(" je");
+        printf(" petak");
+        printf(" moj");
+        printf(" omiljeni");
+        printf(" dan \n");    
     }
 
     return 0;
@@ -62,19 +67,28 @@ void* print3 (void *lpParam)
 
 int main (void)
 {
-    pthread_t hPrint1;
-    pthread_t hPrint2;
-    pthread_t hPrint3;
-
-    pthread_mutex_init(&cs_mutex, NULL);
+    sem_init(&startSemaphore1, 0, 0);
+    sem_init(&startSemaphore2, 0, 0);
+    sem_init(&startSemaphore3, 0, 0);
 
     pthread_create(&hPrint1, NULL, print1, 0);
     pthread_create(&hPrint2, NULL, print2, 0);
     pthread_create(&hPrint3, NULL, print3, 0);
 
-    getchar();
+    /* Signaliziranje nitima da mogu da pocnu sa radom.
+    Ovim se osigurava da ni jedna nit ne dodje do funkcije pthread_join
+    a da nit koju zeli da prikljuci nije pokrenuta */
 
-    pthread_mutex_destroy(&cs_mutex);
+    sem_post(&startSemaphore1);
+    sem_post(&startSemaphore2);
+    sem_post(&startSemaphore3);
+
+    /* Prikljuci se na nit 1 */
+    pthread_join(hPrint1, NULL);
+
+    sem_destroy(&startSemaphore1);
+    sem_destroy(&startSemaphore2);
+    sem_destroy(&startSemaphore3);
 
     return 0;
 }
